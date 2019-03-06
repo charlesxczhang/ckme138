e034f9b153b7ac3288ad5a37db850d855c1b774f
charlesxczhang <38308443+charlesxczhang@users.noreply.github.com>
2019-03-06 15:01:23 -0500

    Add files via upload

---
+++ Consolidate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "date,open,high,low,close,volume,Name" > all_stocks_5yr.csv
+cd individual_stocks_5yr
+files=$(ls *.csv)
+for file in $files
+do
+	tail -n +2 $file >> ../all_stocks_5yr.csv
+done
+© 2018 GitHub, Inc.
